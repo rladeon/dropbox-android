12f37d74eb0e5de6ab65d69c4a8f7095ee6d34e0
rladeon <rladeon@gmail.com>
2016-05-12 16:41:12 -0400

    add connect & unconnected icon

--- src/main/java/com/dropbox/core/examples/android/UserActivity.java
+++ src/main/java/com/dropbox/core/examples/android/UserActivity.java
@@ -1,14 +1,20 @@
 package com.dropbox.core.examples.android;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.dropbox.core.android.Auth;
 import com.dropbox.core.v2.users.FullAccount;
+
+import java.lang.reflect.Field;
 
 
 /**
@@ -53,12 +59,14 @@ public class UserActivity extends DropboxActivity {
             findViewById(R.id.name_text).setVisibility(View.VISIBLE);
             findViewById(R.id.type_text).setVisibility(View.VISIBLE);
             findViewById(R.id.files_button).setEnabled(true);
+
         } else {
             findViewById(R.id.login_button).setVisibility(View.VISIBLE);
             findViewById(R.id.email_text).setVisibility(View.GONE);
             findViewById(R.id.name_text).setVisibility(View.GONE);
             findViewById(R.id.type_text).setVisibility(View.GONE);
             findViewById(R.id.files_button).setEnabled(false);
+
         }
     }
 
@@ -78,5 +86,56 @@ public class UserActivity extends DropboxActivity {
             }
         }).execute();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_user, menu);
+        return true;
+    }
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        // enable visible icons in action bar
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Field field = menu.getClass().
+                            getDeclaredField("mOptionalIconsVisible");
+                    field.setAccessible(true);
+                    field.setBoolean(menu, true);
+                } catch (Exception e) {
+                    //Logger.w(TAG, "onMenuOpened(" + featureId + ", " + menu + ")", e);
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_exit:
+                // User chose the "Settings" item, show the app settings UI...
+                finish();
+                moveTaskToBack(true);
+                return true;
 
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        MenuItem unregister = menu.findItem(R.id.action_not_logged);
+        MenuItem register = menu.findItem(R.id.action_logged);
+        if(hasToken())
+        {
+            register.setVisible(true);
+        }
+        else
+        {
+            unregister.setVisible(true);
+        }
+        return true;
+    }
 }
